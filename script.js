c6d81bd9a96939c17094f436a97154741b3f5fe8
Alberto <albepiazzo@gmail.com>
2022-09-20 17:31:43 +0200

    change JS

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 "use strict";
 
-const arrowDown = document.querySelector(".arrow-down");
-const projects = document.querySelector("#projects");
-
-arrowDown.addEventListener("click", function () {
-  allBox.forEach((box) => {
-    sectionObserver.observe(box);
-    box.classList.remove("box--hidden");
-  });
-  projects.scrollIntoView({ behavior: "smooth" });
-});
-
 //// REVEALING ELEMENTS ON SCROOL
 
 const allBox = document.querySelectorAll(".box");
